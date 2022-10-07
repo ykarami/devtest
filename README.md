8dd6b51c56d2d54ac87f9c64dd92a9b7e6d9b89a
ykarami <karami.yassine@gmail.com>
2022-10-07 15:03:47 +0000

    modifi readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # devtest
+didi
--- app/pages/stagiaires.php
+++ app/pages/stagiaires.php
@@ -1,97 +1,122 @@
 <?php
-    require_once('connexiondb.php');
+        require_once("connexiondb.php");
+        ini_set('display_errors', 'on');
+                
+        $filiere = isset($_GET['filiere'])?$_GET['filiere']:0;
+        $nomPrenom = isset($_GET['nomPrenom'])?$_GET['nomPrenom']:"";
+        
 
-    $choice = isset($_GET['choice'])?$_GET['searchC']:"";
-    $search = isset($_GET['search'])?$_GET['choice']:"";
+        $size = isset($_GET['size'])?$_GET['size']:10;
+        $page = isset($_GET['page'])?$_GET['page']:1;
+        $offset = ($page-1)*$size;
 
 
-    if($choice=="idS"){
-        $requet = "SELECT * FROM stagiaire WHERE idStagiaire=$search";
-    }elseif($choice=="prenomS"){
-        $requet = "SELECT * FROM stagiaire WHERE nom=$search";
-    }else{
-        $requet = "SELECT * FROM stagiaire WHERE prenom=$search"; 
-    }
+        $requetFiliere = "SELECT * FROM filiere LIMIT 8 ";
 
+        if($filiere===0){
+            $requestStagiaire = "SELECT idStagiaire,nom,prenom,NomFiliere,civilite FROM filiere as f,stagiaire as s WHERE f.idFiliere=s.idFiliere 
+                                AND(nom like '%$nomPrenom%' OR prenom LIKE '%$nomPrenom%') ORDER BY idStagiaire LIMIT $size offset $offset";
+                                 
+            $requestStagiaireCount = "SELECT COUNT(*) countS FROM stagiaire WHERE nom like '%$nomPrenom%' OR prenom like '%$nomPrenom%'";                       
+        }else{
+            $requestStagiaire = "SELECT idStagiaire,nom,prenom,NomFiliere,civilite FROM filiere as f,stagiaire as s WHERE f.idFiliere=s.idFiliere 
+                                AND(nom like '%$nomPrenom%' OR prenom LIKE '%$nomPrenom%') AND f.idFiliere=$filiere ORDER BY idStagiaire limit $size offset $offset ";
     
+            $requestStagiaireCount = "SELECT COUNT(*) countS FROM stagiaire WHERE (nom like '%$nomPrenom%' OR prenom like '%$nomPrenom%') AND idFiliere=$filiere ";                    
+        }
 
+        $resultatFiliere = $pdo->query($requetFiliere);
 
+        $resultatStagiaire = $pdo->query($requestStagiaire);
+        $resultatStagiaireCount = $pdo->query($requestStagiaireCount);
 
-
-
-
-
-
-
-    $requetAll = "SELECT * FROM stagiaire";
-    $resultatAll = $pdo->query($requetAll);
-
-
-    
-
-
+        $tabCount = $resultatStagiaireCount->fetch();
+        $nbrS = $tabCount['countS'];
+        $reste = $nbrF%$size;
+        if($reste==0){
+            $nbrPage = $nbrS/$size;
+        }else{
+            $nbrPage = floor($nbrS/$size)+1;
+        }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Gestion des filièere</title>
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css"> 
+    <link rel="stylesheet" type="text/css" href="../css/style.css">     
 </head>
 <body>
     <?php include("menu.php"); ?>
 
         <div class="container">    
             <div class="panel panel-default margetop">
-                <div class="panel-heading">Rechercher des Stagiaires</div>
+                <div class="panel-heading">Rechercher des stagiares</div>
                 <div class="panel-body">
-                    <div>
-                        <form method="get" action="stagiaires.php" class="form-inline ">
-                            <div class="form-group">
-                            <label for="Choose">Chercher par :</label>
-                                <select name="choice" class="form-control" id="choose">
-                                    <option value="idS" selected>ID</option>
-                                    <option value="prenomS" >Prénom</option>
-                                    <option value="NnomS">Nom</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="tet">===></label>
-                                <input type="text" name="search" class="form-control" id="tet">
-                            </div>&nbsp
-                            <div class="form-group">
-                                <button class="btn btn-success">Rechercher..</button>
-                            </div>    
-                        </form>
-                    </div>
-
+                    <form method="get" action="stagiaires.php"class="form-inline">
+                        <div class="form-group" >
+                            <label for="filiere">Filiere : </label>
+                            <select name="filiere" id="filiere" class="form-control">
+                                <option value="0">Toutes les filieres </option>
+                                <?php while($filiere=$resultatFiliere->fetch()){  ?>
+                                <option value="<?php echo $filiere['idFiliere']?>"><?php echo $filiere['NomFiliere']?></option>
+                                <?php }?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="nomPrenom" placeholder="Tapez le nom et le prénom" class="form-control" value="<?php echo $nomPrenom ?>">
+                        </div>
+                        <button type="submit" class="btn btn-success" > <span class="glyphicon glyphicon-search"></span> Chercher...</button>
+                        &nbsp &nbsp
+                        <a href="newFiliere.php"><span class="glyphicon glyphicon-plus"> </span> Nouvelle filiere</a>
+                    </form>
                 </div>
             </div>
             <div class="panel panel-primary margetop">
-                <div class="panel-heading">Liste des Stagiaires</div>
+                <div class="panel-heading">Liste des Stagiaires(<?php echo $nbrS ?>) Stagiaires</div>      
                 <div class="panel-body">
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                            <th>ID</th><th>Prénom</th><th>Nom</th><th>Civilité</th>
+                            <th>id Stagiaire</th><th>Prénom </th><th>Nom</th><th>Civilité</th><th>Filière</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php while($stagiaire = $resultatAll->fetch()){ ?>
-                                <tr>
-                                    <td><?php echo $stagiaire['idStagiaire'] ?></td>
-                                    <td><?php echo $stagiaire['prenom'] ?></td>
-                                    <td><?php echo $stagiaire['nom'] ?></td>
-                                    <td><?php echo $stagiaire['civilite'] ?></td>
-                                </tr>
-                             <?php } ?>   
+                        <?php while($stagiaire=$resultatStagiaire->fetch()){?>
+                            <tr>
+                                <td><?php echo $stagiaire['idStagiaire'] ?></td>
+                                <td><?php echo $stagiaire['prenom']?></td>
+                                <td><?php echo $stagiaire['nom']?></td>
+                                <td><?php echo $stagiaire['civilite']?></td>
+                                <td><?php echo $stagiaire['NomFiliere']?></td>
+
+                                <td>
+                                    <a href="editFiliere.php?idFiliere=<?php echo $filiere['idFiliere']?>">
+                                        <span class="glyphicon glyphicon-edit"> </span>
+                                    </a>
+                                     &nbsp 
+                                     <a onclick="return confirm(' etes vous sur de suprimer')" href="deleteFiliere.php?idF=<?php echo $filiere['idFiliere'] ?>" >
+                                        <span class="glyphicon glyphicon-trash"> </span>
+                                     </a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+
                         </tbody>
-
-
                     </table>
-                </div>
+                    <div >
+                        <ul class="pagination">
+                        <?php  for($i=1; $i<=$nbrPage;$i++){ ?>
+                            
+                             
+                            <li class="<?php if($i==$page) echo 'active' ?>"><a href="filieres.php?page=<?php echo $i;?>&nomF=<?php echo $nomF ?>&niveau=<?php echo $niveau ?>">  <?php echo $i; ?> </a></li>
+			<?php } ?>  
+
+                        </ul>
+                          
+                    </div>
+                </div>   
             </div>
         </div>   
 
